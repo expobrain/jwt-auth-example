b15845738e467530111f82b9fc56cc33b07a3cf4
Daniele Esposti <daniele.esposti@gmail.com>
2016-04-03 23:16:27 +0100

    Implemented Authentication and API server

--- api_server.py
+++ api_server.py
@@ -1,11 +1,37 @@
 from __future__ import unicode_literals
 
-from flask import Flask
+from flask import Flask, request, abort
+import jwt
 
 
 app = Flask(__name__)
 app.config.from_object('settings.shared')
 app.config.from_object('settings.api')
+
+
+@app.route("/greetings/")
+def greetings():
+    # Get header
+    authorization_header = request.headers.get('Authorization')
+
+    if not authorization_header:
+        abort(400, 'Missing Authorization header')
+
+    if not authorization_header.startswith('Bearer'):
+        abort(400, 'Invalid token type')
+
+    # Get token
+    type_, token = authorization_header.split()
+
+    try:
+        data = jwt.decode(token, app.config['SECRET_KEY'])
+    except jwt.DecodeError:
+        abort(400, 'Invalid token')
+
+    # Return response
+    output = 'Hello {}'.format(data['user'])
+
+    return output
 
 
 if __name__ == '__main__':
--- auth_server.py
+++ auth_server.py
@@ -1,11 +1,28 @@
 from __future__ import unicode_literals
 
-from flask import Flask
+import json
+
+from flask import Flask, request
+import jwt
 
 
 app = Flask(__name__)
 app.config.from_object('settings.shared')
 app.config.from_object('settings.auth')
+
+
+@app.route("/", methods=['POST', 'GET'])
+def authenticate():
+    data = request.json
+    token = jwt.encode(
+        {'user': data['username']},
+        app.config['SECRET_KEY']
+    )
+    output = json.dumps({'token': token})
+
+    return output, 200, {
+        'Content-Type': 'application/json'
+    }
 
 
 if __name__ == '__main__':
